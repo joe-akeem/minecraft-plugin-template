d5166752c5cbfaf80e2380e4797943374e35ffc8
Lasse <lasse.lengacher@gmx.de>
2019-05-12 10:21:10 +0200

    Neues Fakel-Kommando.

--- src/de/joeakeem/plugin/Main.java
+++ src/de/joeakeem/plugin/Main.java
@@ -1,9 +1,6 @@
 package de.joeakeem.plugin;
 
-import de.joeakeem.plugin.commands.BogenCommand;
-import de.joeakeem.plugin.commands.DiamonAxeCommand;
-import de.joeakeem.plugin.commands.EnrichCommand;
-import de.joeakeem.plugin.commands.PfeilComand;
+import de.joeakeem.plugin.commands.*;
 import de.joeakeem.plugin.event.MovementListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -16,5 +13,6 @@ public class Main extends JavaPlugin {
         this.getCommand("hack").setExecutor(new DiamonAxeCommand());
         this.getCommand("bogen").setExecutor(new BogenCommand());
         this.getCommand("pfeil").setExecutor(new PfeilComand());
+        this.getCommand("fakel").setExecutor(new FakelComand());
     }
 }
